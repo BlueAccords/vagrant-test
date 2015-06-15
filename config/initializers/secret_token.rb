751260cc558a4b7e27b5c89ab01849e8f37d9acb
BlueAccords <myoldemailname@gmail.com>
2015-06-15 20:37:15 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DaredevilApp::Application.config.secret_key_base = '5f180d9fa0e3786f7a099e2a7fe586e94597756974fff37c69d96bbd1e62ce13240deb9bb35b483ea03b959f0c69bc08f4f47b5e755419aceba30b762e46fe17'
